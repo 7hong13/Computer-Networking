6f4361dfc982f7417cb03e2495d6ec081d71006a
7hong13 <ledi000@naver.com>
2021-06-08 19:46:55 +0900

    crc encoder implementation

--- CRC/crc_encoder/crc_encoder.cpp
+++ CRC/crc_encoder/crc_encoder.cpp
@@ -1,47 +1,120 @@
+#include <stdio.h>
 #include <iostream>
-#include <cstdlib>
+#include <stdlib.h>
+#include <bitset>
+#include <string>
+#include <algorithm>
+#define ARGS_CNT_ERR 0
+#define INPUT_ERR 1
+#define OUTPUT_ERR 2
+#define DATAWORD_SIZE_ERR 3
 using namespace std;
 
-void exitArgsErr() {
-    fprintf(stderr, "usage: ./crc_encoder input_file output_file generator dataword_size\n");
+void exitErr(int err) {
+    if (err == ARGS_CNT_ERR) {
+        fprintf(stderr, "usage: ./crc_encoder input_file output_file generator dataword_size\n");
+    }
+    else if (err == INPUT_ERR) {
+        fprintf(stderr, "input file open error.\n");
+    }
+    else if (err == OUTPUT_ERR) {
+        fprintf(stderr, "output file open error.\n");
+    }
+    else if (err == DATAWORD_SIZE_ERR) {
+        fprintf(stderr, "dataword size must be 4 or 8.\n");
+    }
     exit(1);
 }
 
-void exitInputErr() {
-    fprintf(stderr, "input file open error.\n");
-    exit(1);
-}
+string getMod(string dataword, string generator) {
+    int gen_len = generator.length();
+    int lastIdx = gen_len;
+    string dividend = dataword.substr(0, gen_len);
+    string rem = "";
+    for (int iter = 0; iter < gen_len - 1; iter++) {
+        dataword += '0';
+    }
 
-void exitOutputErr() {
-    fprintf(stderr, "output file open error.\n");
-    exit(1);
-}
+    int len = dataword.length();
+    while (lastIdx < len) {
+        if (dividend[0] == '0') {
+            dividend = dividend.substr(1, gen_len) + dataword[lastIdx++];
+        }
+        else {
+            string res = "";
+            for (unsigned idx = 1; idx < dividend.length(); idx++) {
+                res += dividend[idx] == generator[idx] ? '0' : '1';
+            }
+            dividend = res;
+            dividend += dataword[lastIdx++];
+        }
+    }
 
-void exitDatawordErr() {
-    fprintf(stderr, "dataword size must be 4 or 8.\n");
-    exit(1);
+    //rem 구하기
+    if (dividend[0] == '0') {
+            rem = dividend.substr(1, gen_len);
+    }
+    else {
+        for (unsigned idx = 1; idx < dividend.length(); idx++) {
+            rem += dividend[idx] == generator[idx] ? '0' : '1';
+        }
+    }
+    return rem;
 }
 
 int main(int argc, char* argv[]) {
     if (argc != 5) {
-        exitArgsErr();
+        exitErr(ARGS_CNT_ERR);
     }
 
     FILE *input_fp, *output_fp;
     int dataword_size = atoi(argv[4]);
     input_fp = fopen(argv[1], "r"); //input file open
     if (input_fp == NULL) {
-        exitInputErr();
+        exitErr(INPUT_ERR);
     }
 
     output_fp = fopen(argv[2], "r"); //output file open
     if (output_fp == NULL) {
-        exitOutputErr();
+        exitErr(OUTPUT_ERR);
     }
 
     if (dataword_size!= 4 && dataword_size != 8) {
-        exitDatawordErr();
+        exitErr(DATAWORD_SIZE_ERR);
     }
 
+    char input; 
+    string dataword, codeword;
+    fscanf(input_fp, "%c", &input);
+    while (!feof(input_fp)) {
+        string dec_to_binary = bitset<8>(int(input)).to_string();
+        output_fp = fopen(argv[2], "w");
+        if (dataword_size == 8) {
+            dataword = dec_to_binary;
+            codeword = dataword + getMod(dataword, argv[3]);
+        }
+        else if (dataword_size == 4) {
+            for (int start = 0; start < 5; start += 4) {
+                dataword = dec_to_binary.substr(start, 4);
+                codeword += dataword + getMod(dataword, argv[3]);
+            }
+        }
+        fscanf(input_fp, "%c", &input);
+    }
+    int padding = 0;
+    if (codeword.length() % 8 != 0) {
+        padding = 8 - codeword.length() % 8;
+        for (int iter = 0; iter < padding; iter++) {
+            codeword = '0' + codeword;
+        }
+    }
+    string padding_bit = bitset<8>(padding).to_string();
+    string total_input = padding_bit + codeword;
+    for (unsigned idx = 0; idx < total_input.length(); idx += 8) {
+        fprintf(output_fp, "%c", (int) strtoull(total_input.substr(idx, 8).c_str(), NULL, 2));
+    }
+
+    fclose(input_fp);
+    fclose(output_fp);
     return 0;
 }

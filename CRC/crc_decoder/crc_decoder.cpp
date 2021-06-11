def0cd5040b36002b14e43967af3032fa6608666
7hong13 <ledi000@naver.com>
2021-06-11 15:00:37 +0900

    crc decoder implementation

--- CRC/crc_decoder/crc_decoder.cpp
+++ CRC/crc_decoder/crc_decoder.cpp
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <bitset>
 #include <string>
-#include <algorithm>
+#include <cstring>
 #define ARGS_CNT_ERR 0
 #define INPUT_ERR 1
 #define OUTPUT_ERR 2
@@ -13,7 +13,7 @@ using namespace std;
 
 void exitErr(int err) {
     if (err == ARGS_CNT_ERR) {
-        fprintf(stderr, "usage: ./crc_encoder input_file output_file generator dataword_size\n");
+        fprintf(stderr, "usage: ./crc_decoder input_file output_file result_file generator dataword_size\n");
     }
     else if (err == INPUT_ERR) {
         fprintf(stderr, "input file open error.\n");
@@ -28,6 +28,43 @@ void exitErr(int err) {
         fprintf(stderr, "result file open error.\n");
     }
     exit(1);
+}
+
+bool hasCodewordErr(string codeword, string generator) {
+    int gen_len = generator.length();
+    int lastIdx = gen_len;
+    string dividend = codeword.substr(0, gen_len);
+    string rem = "";
+
+    int len = codeword.length();
+    while (lastIdx < len) {
+        if (dividend[0] == '0') {
+            dividend = dividend.substr(1, gen_len) + codeword[lastIdx++];
+        }
+        else {
+            string res = "";
+            for (unsigned idx = 1; idx < dividend.length(); idx++) {
+                res += dividend[idx] == generator[idx] ? '0' : '1';
+            }
+            dividend = res;
+            dividend += codeword[lastIdx++];
+        }
+    }
+
+    //rem 구하기
+    if (dividend[0] == '0') {
+            rem = dividend.substr(1, gen_len);
+    }
+    else {
+        for (unsigned idx = 1; idx < dividend.length(); idx++) {
+            rem += dividend[idx] == generator[idx] ? '0' : '1';
+        }
+    }
+    
+    for (unsigned idx = 0; idx < rem.length(); idx++) {
+        if (rem[idx] != '0') return true;
+    }
+    return false;
 }
 
 int main(int argc, char* argv[]) {
@@ -56,6 +93,42 @@ int main(int argc, char* argv[]) {
         exitErr(DATAWORD_SIZE_ERR);
     }
 
-    
+    char char_padding; 
+    int padding;
+    fscanf(input_fp, "%c", &char_padding);
+    padding = int(char_padding);
+
+    char input;
+    string bytes, codeword, dataword = "";
+    int codeword_cnt = 0;
+    int codeword_err = 0;
+    int gen_len = strlen(argv[4]);
+    int codeword_len = dataword_size + gen_len - 1;
+    int dataword_to_dec;
+    output_fp = fopen(argv[2], "w");
+
+    fscanf(input_fp, "%c", &input);
+    while (!feof(input_fp)) {
+        bytes += bitset<8>(int(input)).to_string();
+        fscanf(input_fp, "%c", &input);
+    }
+
+    for (unsigned idx = padding; idx < bytes.length(); idx += codeword_len) {
+        codeword = bytes.substr(idx, codeword_len);
+        if (hasCodewordErr(codeword, argv[4])) codeword_err++;
+        codeword_cnt++;
+        dataword += codeword.substr(0, dataword_size);
+        if (dataword.length() != 8) continue; 
+        dataword_to_dec = (int) strtoull(dataword.c_str(), NULL, 2);
+        fprintf(output_fp, "%c", dataword_to_dec);
+        dataword = "";
+    }
+
+    result_fp = fopen(argv[3], "w");
+    fprintf(result_fp, "%d %d", codeword_cnt, codeword_err);
+
+    fclose(input_fp);
+    fclose(output_fp);
+    fclose(result_fp);
     return 0;
 }
